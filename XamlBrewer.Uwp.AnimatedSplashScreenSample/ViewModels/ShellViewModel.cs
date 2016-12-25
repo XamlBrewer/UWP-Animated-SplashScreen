843bfe01ec3680a65b19a4954d5d7e909d0115b0
Diederik Krols <diederik_krols@hotmail.com>
2016-12-25 11:39:20 +0100

    Assets and Drill-In.

--- XamlBrewer.Uwp.AnimatedSplashScreenSample/ViewModels/ShellViewModel.cs
+++ XamlBrewer.Uwp.AnimatedSplashScreenSample/ViewModels/ShellViewModel.cs
@@ -10,9 +10,7 @@ namespace Mvvm
             // Build the menu
             // Symbol enumeration is here: https://msdn.microsoft.com/en-us/library/windows/apps/windows.ui.xaml.controls.symbol.aspx
             Menu.Add(new MenuItem() { Glyph = Symbol.Home, Text = "Home", NavigationDestination = typeof(MainPage) });
-            //Menu.Add(new MenuItem() { Glyph = Symbol.Message, Text = "FlyOut", NavigationDestination = typeof(FlyOutPage) });
-            //Menu.Add(new MenuItem() { Glyph = Symbol.PreviewLink, Text = "Dialog", NavigationDestination = typeof(DialogPage) });
-            //Menu.Add(new MenuItem() { Glyph = Symbol.ViewAll, Text = "Squares", NavigationDestination = typeof(SquareOfSquaresPage) });
+            Menu.Add(new MenuItem() { Glyph = Symbol.Street, Text = "Other", NavigationDestination = typeof(OtherPage) });
         }
     }
 }
--- XamlBrewer.Uwp.AnimatedSplashScreenSample/Views/OtherPage.xaml.cs
+++ XamlBrewer.Uwp.AnimatedSplashScreenSample/Views/OtherPage.xaml.cs
@@ -18,9 +18,9 @@ namespace XamlBrewer.Uwp.AnimatedSplashScreenSample
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class OtherPage : Page
     {
-        public MainPage()
+        public OtherPage()
         {
             this.InitializeComponent();
         }
